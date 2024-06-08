f47a7d4931f1710cf51b4e66dfcd7e19033a26fc
Ivan Skliaruk <cunanori@gmail.com>
2024-06-08 05:54:04 +0200

    hw3 Course viewsets and permissions adjustments

--- materials/urls.py
+++ materials/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 
 from materials.apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
-from materials.views import LessonListCreateAPIView, CourseListCreateAPIView, CourseRetrieveUpdateDestroyAPIView, \
-    LessonRetrieveUpdateDestroyAPIView
-from users.views import PaymentListAPIView, CourseViewSet
+from materials.views import LessonListCreateAPIView, LessonRetrieveUpdateDestroyAPIView
+from users.views import CourseViewSet
 
 app_name = MaterialsConfig.name
 
